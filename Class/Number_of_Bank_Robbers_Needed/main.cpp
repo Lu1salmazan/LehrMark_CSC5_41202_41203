6cc447e85d8a513e14952eb1626778a81b6c0675
ml1150258 <mark@omniplatform.com>
2016-01-07 10:26:04 -0800

    Signed-off-by: ml1150258 <mark@omniplatform.com>

--- Class/Number_of_Bank_Robbers_Needed/main.cpp
+++ Class/Number_of_Bank_Robbers_Needed/main.cpp
@@ -11,8 +11,8 @@ using namespace std;
 //User Libraries
 
 //Global Constant
-const char  wtBill=1;//Weight in grams
-const float cnvLbs=1.0f/453.5f;//Conversion from grams to lbs
+const char  WTBILL=1;//Weight in grams
+const float CNVLBS=1.0f/453.5f;//Conversion from grams to lbs
 
 //Function Prototypes
 
@@ -31,7 +31,8 @@ int main(int argc, char** argv) {
     cin>>denom;
     
     //Calculate the number of fellow perpetrators 
-    nPerps=cnvLbs*amtStl*wtBill/denom/wtPers+1;
+    nPerps=CNVLBS*amtStl*WTBILL/denom/wtPers;//Truncation Equation
+    nPerps=nPerps+1;//Untruncated Equation
     
     //Output the results
     cout<<"Amount Desired = $"<<amtStl<<endl;
