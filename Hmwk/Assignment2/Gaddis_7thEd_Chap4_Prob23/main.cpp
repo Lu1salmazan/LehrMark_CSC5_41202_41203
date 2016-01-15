cb26c4c3d3eb7760fad6f866531a91e2272f424c
ml1150258 <mark.lehr@rcc.edu>
2016-01-14 21:46:57 -0800

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

---
+++ Hmwk/Assignment2/Gaddis_7thEd_Chap4_Prob23/main.cpp
@@ -0,0 +1,33 @@
+/* 
+    File:   main.cpp
+    Author: Dr. Mark E. Lehr
+    Created on January 4, 2016, 10:18 AM
+    Purpose:  Check out IDE, Helloworld
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    
+    //Input data
+    
+    //Calculate or map inputs to outputs
+    
+    //Output the results
+    cout<<"Hello World"<<endl;
+    int dum;
+    cin>>dum;
+
+    //Exit stage right
+    return 0;
+}
--- Hmwk/Assignment2/Savitch_8thEd_Chap2_Prob6/main.cpp
+++ Hmwk/Assignment2/Savitch_8thEd_Chap2_Prob6/main.cpp
@@ -1,12 +1,13 @@
 /* 
     File:   main.cpp
     Author: Dr. Mark E. Lehr
-    Created on January 4, 2016, 10:18 AM
-    Purpose:  Check out IDE, Helloworld
+    Created on January 44, 2016, 9:05 PM
+    Purpose:  ISP Bill
  */
 
 //System Libraries
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 //User Libraries
@@ -18,16 +19,58 @@ using namespace std;
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare and initialize variables
+    char package;      //A,B,C
+    unsigned short hrs;//Hours utilized
+    float bill;        //Your bill in $'s
+    bool sentnl=true;  //End loop when sentnl is false
     
-    //Input data
-    
-    //Calculate or map inputs to outputs
-    
-    //Output the results
-    cout<<"Hello World"<<endl;
-    int dum;
-    cin>>dum;
+    //Loop until you end with a sentinels
+    do{
+        //Initialize the Bill
+        bill=0;
+        //Input data
+        cout<<"Calculate your ISP Bill"<<endl;
+        cout<<"What package are you under A, B, or C"<<endl;
+        cin>>package;
+        if(toupper(package)=='A'||toupper(package)=='B'){
+            cout<<"How many hours have you utilized"<<endl;
+            cin>>hrs;
+        }
 
+        //Calculate or map inputs to outputs, i.e. calculate the bill
+        switch(package){
+            case 'A':
+            case 'a':{
+                if(hrs<=10)bill=9.95f;
+                else bill=9.95f+2*(hrs-10);
+                break;
+            }
+            case 'B':
+            case 'b':{
+                if(hrs<=20)bill=14.95f;
+                else bill=14.95f+(hrs-20);
+                break;
+            }
+            case 'C':
+            case 'c':{
+                bill=19.95f;
+                break;
+            }
+            default: cout<<"Bad package, no bill can be computed!"<<endl;
+                     sentnl=false;
+            };
+        
+        //Output the results
+        cout<<fixed<<setprecision(2)<<showpoint;
+        if(tolower(package)=='a'||tolower(package)=='b'){
+            cout<<"For package "<<package<<" and hours = "
+                <<hrs<<" the Bill = $"<<bill<<endl;
+        }else{
+            cout<<"For package "<<package
+                <<" the Bill = $"<<bill<<endl;
+        }
+    }while(sentnl);
+    
     //Exit stage right
     return 0;
 }
