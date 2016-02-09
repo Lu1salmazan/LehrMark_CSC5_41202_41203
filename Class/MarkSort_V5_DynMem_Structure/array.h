d76a0af4919187201e49c7a0f45e62f2b9364f6e
ml1150258 <mark.lehr@rcc.edu>
2016-02-09 10:18:48 -0800

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

---
+++ Class/MarkSort_V5_DynMem_Structure/array.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   array.h
+ * Author: rcc
+ *
+ * Created on February 9, 2016, 9:50 AM
+ */
+
+#ifndef ARRAY_H
+#define	ARRAY_H
+
+struct Array{
+    int size;
+    int *data;
+};
+
+#endif	/* ARRAY_H */
+
